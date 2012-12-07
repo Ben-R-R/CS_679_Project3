fb641eae4df6325d1df8ae33da6ff53c235137f8
Ben-R-R <tolkienfan72@gmail.com>
2012-12-07 01:37:24 -0600

    adjusted queue tutorial

--- globals.js
+++ globals.js
@@ -3,9 +3,10 @@
 //level paths
 var currLevel = 0;
 var levels = [];
-levels.push("levels/queueTutorial.svg");
+
 levels.push("levels/kangarooTutorial.svg");
-levels.push("levels/spiderTutorial.svg"); 
+levels.push("levels/spiderTutorial.svg");
+levels.push("levels/queueTutorial.svg"); 
 levels.push("levels/test4.svg");
 levels.push("levels/test6.svg");
 //console.log(levels[0]+" "+levels[1]);
