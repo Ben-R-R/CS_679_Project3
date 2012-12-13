0240be3f495c0f681376099719b35f057cf233d8
Alex Evans <aeiou_11@live.com>
2012-12-13 16:19:39 -0600

    Updated controls and queue display
    -Jump moved to space
    -Queue mode now enabled all the time
    -Fill queue changed:
    --'Q' is human
    --'W' is cheetah
    --'E' is flying squirrel
    --'R' is kangaroo
    --'T' is spider
    -Queue image with letters moved to 'Esc' instead of 'G'
    -'Backspace' now pops last element from power queue
    
    Reasons for changes: playtest feedback -- space is more intuitive for
    jump, one less button to press if we enable queue all the time, moved
    powers above 'F' as it's easier to press 'Q->T' than 'Z->B'

--- Interface.js
+++ Interface.js
@@ -38,20 +38,94 @@ var pauseScreen = {
 	
 	draw: function(){
 		if(this.paused){
+			
+			var dCI = function(img,x,y,scale){
+				theContext.drawImage(img, x - img.width/2, y - img.height/2 , img.width * scale , img.height * scale);
+			}
+			
 			theContext.globalAlpha = 0.5;
 			theContext.fillStyle = "#FFFFFF";
 			theContext.font= Math.floor(theCanvas.height * 0.1) +"px Arial";
+			
 			var tempLineWidth = theContext.lineWidth;
-		
+			var tempAlign = theContext.textAlign;
+			var gutter = 10;
+			var imageWidth = 72;
+			var imageHeight = 72;	
+			
 			theContext.lineWidth = 2.5;
 			theContext.textAlign="center"; 
 			theContext.fillRect(0, 0, theCanvas.width, theCanvas.height);
 			theContext.globalAlpha = 1;	
 			theContext.fillStyle = "#FFFFFF";
 			theContext.strokeStyle = "#000000";
-			theContext.fillText("Paused, press Esc to resume",theCanvas.width/2,theCanvas.height/2);
-			theContext.strokeText("Paused, press Esc to resume",theCanvas.width/2,theCanvas.height/2 );
-			theContext.lineWidth =tempLineWidth;  			
+			theContext.fillText("Paused, press Esc to resume",theCanvas.width/2,theCanvas.height/2-70);
+			theContext.strokeText("Paused, press Esc to resume",theCanvas.width/2,theCanvas.height/2-70);
+			
+			//draw the letters for the powers
+			theContext.fillText("Q",
+				theCanvas.width/2 - imageWidth*2.5 - gutter*2,
+				theCanvas.height/2 + imageHeight + theCanvas.height * 0.05);
+			theContext.strokeText("Q",
+				theCanvas.width/2 - imageWidth*2.5 - gutter*2,
+				theCanvas.height/2 + imageHeight + theCanvas.height * 0.05);
+			
+			theContext.fillText("W",
+				theCanvas.width/2 - imageWidth*1.5 - gutter,
+				theCanvas.height/2 + imageHeight + theCanvas.height * 0.05);
+			theContext.strokeText("W",
+				theCanvas.width/2 - imageWidth*1.5 - gutter,
+				theCanvas.height/2 + imageHeight + theCanvas.height * 0.05);
+				
+			theContext.fillText("E",
+				theCanvas.width/2 - imageWidth/2,
+				theCanvas.height/2 + imageHeight + theCanvas.height * 0.05);
+			theContext.strokeText("E",
+				theCanvas.width/2 - imageWidth/2,
+				theCanvas.height/2 + imageHeight + theCanvas.height * 0.05);
+			
+			theContext.fillText("R",
+				theCanvas.width/2 + imageWidth/2 + gutter,
+				theCanvas.height/2 + imageHeight + theCanvas.height * 0.05);
+			theContext.strokeText("R",
+				theCanvas.width/2 + imageWidth/2 + gutter,
+				theCanvas.height/2 + imageHeight + theCanvas.height * 0.05); 
+				
+			theContext.fillText("T",
+				theCanvas.width/2 + imageWidth*1.5 + gutter*2,
+				theCanvas.height/2 + imageHeight + theCanvas.height * 0.05);
+			theContext.strokeText("T",
+				theCanvas.width/2 + imageWidth*1.5 + gutter*2,
+				theCanvas.height/2 + imageHeight + theCanvas.height * 0.05); 
+				
+			
+			//draw icons
+			theContext.lineWidth = 8;
+			
+			theContext.strokeStyle = "#000000";
+			
+			theContext.strokeRect(
+				theCanvas.width/2 - imageWidth*3 - gutter*2.5, 
+				theCanvas.height/2 - imageHeight/2-gutter/2, 
+				imageWidth*5+gutter*5, 
+				imageHeight+gutter );
+			
+			
+			  
+			theContext.fillRect(
+				theCanvas.width/2 - imageWidth*3 - gutter*2.5, 
+				theCanvas.height/2 - imageHeight/2-gutter/2, 
+				imageWidth*5+gutter*5, 
+				imageHeight+gutter );
+					
+			dCI (H_Icon, theCanvas.width/2 - imageWidth*2.5 - gutter*2, theCanvas.height/2, 1);
+			dCI	(C_Icon, theCanvas.width/2 - imageWidth*1.5 - gutter, theCanvas.height/2, 1);	
+		    dCI	(FS_Icon, theCanvas.width/2 - imageWidth/2, theCanvas.height/2, 1);	
+		    dCI	(K_Icon, theCanvas.width/2 + imageWidth/2+gutter, theCanvas.height/2, 1);	
+		    dCI	(S_Icon, theCanvas.width/2 + imageWidth*1.5 + gutter*2, theCanvas.height/2, 1);	
+			
+			theContext.lineWidth = tempLineWidth;
+			theContext.textAlign= tempAlign; 			
 		}
 	
 	}
