93bf7e973243e71ddbf400f913cfd18c9f3c719a
Alex Evans <aeiou_11@live.com>
2012-12-14 13:48:44 -0600

    added a level after the queue tutorial

--- globals.js
+++ globals.js
@@ -12,6 +12,7 @@ levels.push("levels/spiderTutorial.svg");
 levels.push("levels/test4.svg");
 levels.push("levels/test6.svg"); 
 levels.push("levels/queueTutorial.svg");
+levels.push("levels/movinglevel.svg");
 
 //console.log(levels[0]+" "+levels[1]);
 
